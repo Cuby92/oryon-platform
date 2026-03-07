ebc462293ce3662134e1de460dbbf8dafd9d5229
Cuby92 <jakub.bartul@hotmail.com>
2026-03-07 19:43:56 +0100

    Add example text for testing and apply to layout.tsx and page.tsx

--- platform/src/app/[locale]/page.tsx
+++ platform/src/app/[locale]/page.tsx
@@ -1,9 +1,16 @@
 import styles from "./page.module.css";
+import { useTranslations } from 'next-intl';
+import { routing } from '../../i18n/routing';
+
+export async function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
 
 export default function Home() {
+  const t = useTranslations('HomePage');
   return (
     <div className={styles.page}>
-    
+      <h1>{t('Welcome')}</h1>
     </div>
   );
 }
